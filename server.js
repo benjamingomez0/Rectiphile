593f18a9e215452c244869644f8bb6d1c1581503
benjamingomez0 <benjamingomez0@gmail.com>
2019-12-05 20:04:37 -0800

    Add .env.development file add .env variables

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
 const express    = require('express')
+const path       = require('path')
 const app        = express()
 const PORT       = process.env.PORT || 8000
 const bodyParser = require('body-parser');
 
+app.use(express.static(path.join(__dirname,'build')))
+app.use(express.json())
+// app.use('/auth', userController)
 
+
+app.get('/hello', (req,res)=>{
+    res.json({ message: 'world'})
+})
 
 app.listen(PORT, ()=>{
     console.log(`...Listening on port ${PORT}`)
