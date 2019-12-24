6a62b1f9d2d0b4475bc0c7fb0028569303392819
benjamingomez0 <benjamingomez0@gmail.com>
2019-12-24 12:12:26 -0500

    change home color

--- controllers/uploads.js
+++ controllers/uploads.js
@@ -3,7 +3,7 @@ const app        = express()
 const mongoose   = require('mongoose')
 const router     = express.Router()
 const bodyParser = require('body-parser');
-
+const xlsx       = require('xlsx')
 //Model
 const Master = require('../models/Masters')
 const Version = require('../models/Versions')
